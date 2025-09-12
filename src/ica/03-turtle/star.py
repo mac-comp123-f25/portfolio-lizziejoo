ce7279686a5cbb665e0d8cba38d5d6e9c2b0ee5e
lizziejoo <ljoo@macalester.edu>
2025-09-12 10:13:17 -0500

    triangle done

---
+++ src/ica/03-turtle/star.py
@@ -0,0 +1,13 @@
+import turtle
+
+window = turtle.Screen()
+turtle_star = turtle.Turtle()
+
+
+for aColor in ["yellow", "yellow", "yellow", "yellow"]:
+    turtle_star.color(aColor)
+   turtle_star.color(aColor)
+   turtle_star.left(36)
+   turtle_star.forward(100)
+
+window.exitonclick()
