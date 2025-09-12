78eaf9177acdf9ea433b6fa00c5e9c6184740eb6
lizziejoo <ljoo@macalester.edu>
2025-09-12 10:04:14 -0500

    my turtle

---
+++ src/ica/03-turtle/triangle.py
@@ -0,0 +1,13 @@
+import turtle
+
+window = turtle.Screen()
+my_turtle = turtle.Turtle()
+
+window.bgcolor("lavender")
+my_turtle.color("blue")
+my_turtle.forward(50)
+my_turtle.left(45)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(20)
+window.exitonclick() # this line closes the window when user clicks
