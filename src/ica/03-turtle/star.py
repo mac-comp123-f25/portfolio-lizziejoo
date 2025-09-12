3049720431c4c2c6488d59db843ab7519879589b
lizziejoo <ljoo@macalester.edu>
2025-09-12 10:26:16 -0500

    star

--- src/ica/03-turtle/star.py
+++ src/ica/03-turtle/star.py
@@ -2,12 +2,23 @@ import turtle
 
 window = turtle.Screen()
 turtle_star = turtle.Turtle()
+turtle_star.shape("circle")
 
-
-for aColor in ["yellow", "yellow", "yellow", "yellow"]:
+for aColor in ["yellow", "yellow", "yellow", "yellow", "yellow"]:
     turtle_star.color(aColor)
-   turtle_star.color(aColor)
-   turtle_star.left(36)
-   turtle_star.forward(100)
+    turtle_star.right(144)
+    turtle_star.forward(100)
+
+turtle_star.up()
+turtle_star.goto(100, 100)
+turtle_star.down()
+
+for aColor in ["pink", "pink", "pink", "pink", "pink"]:
+    turtle_star.color(aColor)
+    turtle_star.right(144)
+    turtle_star.forward(100)
+
+
+
 
 window.exitonclick()
