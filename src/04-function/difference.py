6dc6d2b1f021979e1cb4bd7bcb296010abfc5e87
lizziejoo <ljoo@macalester.edu>
2025-09-15 10:28:31 -0500

    ave3

--- src/04-function/difference.py
+++ src/04-function/difference.py
@@ -1,8 +1,10 @@
 def smallest_diff(x, y, z):
+    print('smallest_diff inputs:', x, y, z)
     diff1 = abs(x - y)
     diff2 = abs(y - z)
     diff3 = abs(x - z)
     min_diff = min(diff1, diff2, diff3)
+    print(diff1, diff2, diff3, 'return value:', min_diff)
     return min_diff
 
 smallest_diff(32, 43, 90)
---
+++ src/04-function/my_avg.py
@@ -0,0 +1,6 @@
+def ave3(x, y, z):
+    ave = ( x + y + z) / 3
+    return ave
+
+res = ave3(2, 3.0, 4.0)
+print(res)
