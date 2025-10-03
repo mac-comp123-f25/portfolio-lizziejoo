17d6e5b44a036da2d008e394a17447beae0ddc35
lizziejoo <ljoo@macalester.edu>
2025-10-03 09:39:12 -0500

    done every fifth

---
+++ src/08-iteration/whileloops.py
@@ -0,0 +1,10 @@
+def print_every_fifth(x):
+  while x >= 0:  # x is the loop variable
+    print(x)
+    x = x - 5
+  # when indentation stops, while loop is over
+  print("Done!")
+
+print_every_fifth(20)
+print_every_fifth(11)
+
