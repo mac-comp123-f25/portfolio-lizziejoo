b7ee273f10d48cc3703d94bd695a3a76d4ba3807
lizziejoo <ljoo@macalester.edu>
2025-11-05 10:37:20 -0600

    refined reverse

--- src/ica/20-tkinter/reverse.py
+++ src/ica/20-tkinter/reverse.py
@@ -4,22 +4,22 @@ import tkinter as tk
 class BasicGui:
     def __init__(self):
         self.mainWin = tk.Tk()
-        self.mainWin.title("String Reverser")
+        self.mainWin.title("Reverse Text")
 
         instruction_label = tk.Label(self.mainWin, text="Enter a phrase and hit [Enter]:")
-        instruction_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky='w')
+        instruction_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
-        self.input_entry = tk.Entry(self.mainWin, width=40)
+        self.input_entry = tk.Entry(self.mainWin, width=30)
         self.input_entry.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
 
         output_heading_label = tk.Label(self.mainWin, text="Reversed Text:")
-        output_heading_label.grid(row=2, column=0, padx=10, pady=(10, 0), sticky='w')
+        output_heading_label.grid(row=2, column=0, padx=10, pady=(10, 0))
 
         self.display_label = tk.Label(self.mainWin, text="", fg="blue")
-        self.display_label.grid(row=3, column=0, columnspan=2, padx=10, pady=(0, 10), sticky='w')
+        self.display_label.grid(row=3, column=0, columnspan=2, padx=10, pady=(0, 10))
 
         quit_button = tk.Button(self.mainWin, text="Quit", command=self.quit_callback)
-        quit_button.grid(row=4, column=1, padx=10, pady=10, sticky='se')
+        quit_button.grid(row=4, column=1, padx=10, pady=10)
 
         self.input_entry.bind('<Return>', self.entry_response)
 
