bce26f00da268a841c412d6099432ac857fe68b7
lizziejoo <ljoo@macalester.edu>
2025-11-12 10:09:21 -0600

    made files

---
+++ src/ica/16-simple-images/scale_color.py
@@ -0,0 +1,30 @@
+from src.ica.helpers.imageTools import *
+from src.ica.helpers.dummyWindow import *
+
+def grayscale(pic):
+    new_pic = pic.copy()
+    for (x, y) in new_pic:
+        (r, g, b) = new_pic.getColor(x, y)
+        lumin = (r + g + b) / 3
+        new_pic.setColor(x, y, (lumin, lumin, lumin))
+
+    return new_pic
+
+
+
+def weighted_scale(picture, w1, w2, w3):
+    new_pic = duplicatePicture(picture)
+
+    for pixel in getPixels(new_pic):
+        r = getRed(pixel)
+        g = getGreen(pixel)
+        b = getBlue(pixel)
+
+        lumin = w1 * r + w2 * g + w3 * b
+
+        setColor(pixel, makeColor(lumin, lumin, lumin))
+
+    return new_pic
+
+
+
