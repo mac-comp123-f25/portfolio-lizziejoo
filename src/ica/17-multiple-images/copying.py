6c77e230b8faaf434d0776eb0f5a136bba2aba8f
lizziejoo <ljoo@macalester.edu>
2025-11-14 09:53:05 -0600

    copying pic

--- src/ica/17-multiple-images/copying.py
+++ src/ica/17-multiple-images/copying.py
@@ -1,3 +1,6 @@
+from src.ica.helpers.imageTools import *
+from src.ica.helpers.dummyWindow import *
+
 def copy_pic_into(small_pic, big_pic, start_x, start_y):
     for (x,y) in small_pic:
         color = small_pic.getColor(x,y)
@@ -5,3 +8,10 @@ def copy_pic_into(small_pic, big_pic, start_x, start_y):
         target_y = start_y + y
 
         big_pic.setColor(target_x, target_y, color)
+
+green_turtle = Picture("../SampleImages/greenTurtle.jpg")
+scene = Picture("../SampleImages/bearLake.jpg")
+copy_pic_into(green_turtle, scene, 25, 25)
+copy_pic_into(green_turtle, scene, 200, 200)
+copy_pic_into(green_turtle, scene, 400, 200)
+scene.show()
